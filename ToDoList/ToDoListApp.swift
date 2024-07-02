22f112fe971497a1ada12d9f1fd9dc4fdb7e3738
dwivedyaakash <aakashdwd07@gmail.com>
2024-07-03 00:13:54 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Aakash Dwivedy on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
